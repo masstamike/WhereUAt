f14daa544cef116e30089e227f4602cff4f4693c
Michael Sawyer <michaelsawyer92@gmail.com>
2014-05-08 14:42:00 -0700

    Added all the files! Sorry, didn't realize I wasn't pushing up some stuff.

--- work/src/com/example/whereuat/MainActivity.java
+++ work/src/com/example/whereuat/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
