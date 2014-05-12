d85c5d626f81df5a67c75bbcba9a172e26aec890
mt523 <mtostenson@mail.csuchico.edu>
2014-05-11 19:23:00 -0700

    Compass fully working along with other navigation metrics

--- src/com/example/whereuat/NavigationActivity.java
+++ src/com/example/whereuat/NavigationActivity.java
@@ -1,10 +1,16 @@
 package com.example.whereuat;
 
+import java.util.Date;
+
 import com.google.android.gms.maps.model.LatLng;
 
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
 import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -15,28 +21,37 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-public class NavigationActivity extends ActionBarActivity 
+public class NavigationActivity extends ActionBarActivity implements SensorEventListener
 {
+	float currentDegree = 0f;
 	TextView tv1;
 	TextView tv2;
 	TextView tv3;
+	TextView tv4;
+	TextView tv5;
+	TextView tv6;
+	ImageView compass_image;
+	Location dest;	
+    LocationManager mLocationManager;
+    SensorManager mSensorManager;
 
-	Location cur_location;
-	Location prev_location;
-	
-    LocationManager manager;
     LocationListener listener = new LocationListener()
     {
 
 		@Override
 		public void onLocationChanged(Location location) {
 			// Update navigation metrics
-			prev_location = cur_location;
-			cur_location = location;
-			tv2.setText(String.valueOf(location.getLatitude()));
-			tv3.setText(String.valueOf(location.getLongitude()));
+			
+			tv1.setText("dest: " + getIntent().getStringExtra("name"));
+			tv2.setText(String.valueOf("my lat: " + location.getLatitude()));
+			tv3.setText(String.valueOf("my long: " + location.getLongitude()));
+			tv4.setText(String.valueOf("distance: " + location.distanceTo(dest)));
+			tv5.setText(String.valueOf("bearing: " + location.bearingTo(dest)));
 		}
 
 		@Override
@@ -58,30 +73,52 @@ public class NavigationActivity extends ActionBarActivity
 		}    	
     };
 
+	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) 
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_navigation);
-
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
 		
+		// Set views
 		tv1 = (TextView) findViewById(R.id.textView1);
 		tv2 = (TextView) findViewById(R.id.textView2);
 		tv3 = (TextView) findViewById(R.id.textView3);
-		tv1.setText(getIntent().getStringExtra("name"));
-		LatLng destination = new LatLng(getIntent().getDoubleExtra("lat", 0.0), getIntent().getDoubleExtra("long", 0.0));
-		tv2.setText(String.valueOf(destination.latitude));
-		tv3.setText(String.valueOf(destination.longitude));
-//		destination.setLatitude(getIntent().getDoubleExtra("lat", 0));
-//		destination.setLongitude(getIntent().getDoubleExtra("long", 0));
+		tv4 = (TextView) findViewById(R.id.textView4);
+		tv5 = (TextView) findViewById(R.id.textView5);
+		tv6 = (TextView) findViewById(R.id.textView6);	
 		
-		manager = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);
-		cur_location = manager.getLastKnownLocation(LOCATION_SERVICE);
-		manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3, 0, listener);	
+		compass_image = (ImageView) findViewById(R.id.imageView1);
+		
+		// Receive destination location from intent
+		LatLng destination = new LatLng(getIntent().getDoubleExtra("lat", 0.0), getIntent().getDoubleExtra("long", 0.0));
+		dest = new Location("dest");
+		dest.setLatitude(getIntent().getDoubleExtra("lat", 0));
+		dest.setLongitude(getIntent().getDoubleExtra("long", 0));
+		dest.setTime(new Date().getTime());
+		
+		// Setup sensor manager
+		mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+		
+		// Set up location manager
+		mLocationManager = (LocationManager)this.getSystemService(Context.LOCATION_SERVICE);		
+		mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3, 0, listener);	
+	}
+	
+	protected void onResume()
+	{
+		super.onResume();
+		mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION), SensorManager.SENSOR_DELAY_GAME);
+	}
+	
+	protected void onPause()
+	{
+		super.onPause();
+		mSensorManager.unregisterListener(this);
 	}
 
 	@Override
@@ -119,6 +156,32 @@ public class NavigationActivity extends ActionBarActivity
 					container, false);
 			return rootView;
 		}
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor arg0, int arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event) 
+	{
+		float degree = Math.round(event.values[0]);
+		tv6.setText("Compass heading: " + Float.toString(degree) + " degrees");
+		compass_image.setRotation(-degree);
+//		RotateAnimation ra = new RotateAnimation
+//		(
+//			currentDegree,
+//			-degree,
+//			Animation.RELATIVE_TO_SELF, 0.5f,
+//			Animation.RELATIVE_TO_SELF,
+//			0.5f
+//		);		
+//		ra.setDuration(210);		
+//		ra.setFillAfter(true);		
+//		compass_image.startAnimation(ra);
+//		currentDegree = -degree;
 	}
 
 }
