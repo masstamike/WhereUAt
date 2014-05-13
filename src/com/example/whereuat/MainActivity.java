011dd9b7a2e86ec5352fc1ffd948baf6cecd1e98
Michael Sawyer <michaelsawyer92@gmail.com>
2014-05-12 20:37:07 -0700

    Previous

--- src/com/example/whereuat/MainActivity.java
+++ src/com/example/whereuat/MainActivity.java
@@ -2,11 +2,8 @@ package com.example.whereuat;
 
 import android.annotation.TargetApi;
 import android.app.Fragment;
-import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
